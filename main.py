12d54bc578642736da411262d8e8945025ef5923
ava-iannone <ava.iannone.07@gmail.com>
2025-01-21 17:46:31 -0500

    Add files via upload
    Variables

---
+++ main.py
@@ -0,0 +1,19 @@
+First = 5 * 6
+print ("The answer to your query is:", First)
+
+Second = 8 / 3
+print ("The answer to your query is:", Second)
+
+Third = 8 // 3
+print ("The answer to your query is:", Third)
+
+Fourth = 8 % 3
+print ("The answer to your query is:", Fourth)
+
+Fifth = 4200000 + 23090
+print ("The answer to your query is:", Fifth)
+
+Sixth = 4200000 - 7633
+print ("The answer to your query is:", Sixth)
+
+print ("Ava Iannone")
