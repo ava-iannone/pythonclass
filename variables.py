9e62d95599c8849dcdb8bb3528fc674d612cd20d
ava-iannone <ava.iannone.07@gmail.com>
2025-01-26 13:11:03 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,37 @@
+inp1 = int(input("Enter one number to multiply\n"))
+inp2 = int(input("Enter another number to multiply\n"))
+
+inp3 = int(input("\nEnter one number to divide\n"))
+inp4 = int(input("Enter a number to divide by\n"))
+
+inp5 = int(input("\nEnter one number to divide without a remainder\n"))
+inp6 = int(input("Enter a number to divide by\n"))
+
+inp7 = int(input("\nEnter one number to divide\n"))
+inp8 = int(input("Enter a number to divide by and find only the remainder\n"))
+
+inp9 = int(input("\nEnter one number to add\n"))
+inp10 = int(input("Enter another number to add\n"))
+
+inp11 = int(input("\nEnter one number to subtract\n"))
+inp12 = int(input("Enter another number to subtract\n"))
+
+First = inp1 * inp2
+print ("The answer to your query is:", First)
+
+Second = inp3 / inp4
+print ("The answer to your query is:", Second)
+
+Third = inp5 // inp6
+print ("The answer to your query is:", Third)
+
+Fourth = inp7 % inp8
+print ("The answer to your query is:", Fourth)
+
+Fifth = inp9 + inp10
+print ("The answer to your query is:", Fifth)
+
+Sixth = inp11 - inp12
+print ("The answer to your query is:", Sixth)
+
+print ("Ava Iannone")
